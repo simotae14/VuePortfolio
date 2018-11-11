5d33f6350ca7525c97c849689d7012e192d92f7b
Simone Taeggi <simone.taeggi@leovegas.com>
2018-11-12 00:41:45 +0100

    Add Home Html content and some SCSS

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home main-content">
+    <div class="left-content">
+      <h1>
+        5 YRS DESIGN
+      </h1>
+      <p class="subxtext">Simone has been a full stack developer for 3 years and a frontend developer for the last 2 years.</p>
+      <p class="subxtext">I've studied with Udacity, Udemy, Lynda, EdX and more.</p>
+    </div>
+    <div class="right-content img1">
+      <button>
+        <router-link to="portfolio/one">Browse Work</router-link>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -16,3 +26,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .img1 {
+    background: url('../assets/home.jpg')
+  }
+</style>
+
