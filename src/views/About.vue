e6171f0eeb16ff1577b53a2c072cbf902092acc7
Simone Taeggi <startae14@gmail.com>
2018-12-17 00:18:07 +0100

    Add About page html and style

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,48 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about main-content">
+    <div class="left-content">
+      <h1>
+        About me
+      </h1>
+      <p class="subtext">
+        I design, code, and teach a bunch of other people to do the same.
+      </p>
+      <button>
+        <router-link to="/portfolio/one">
+          Look at my work
+        </router-link>
+      </button>
+    </div>
+    <div class="right-content">
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, commodi quidem fuga enim illo explicabo! Maxime et consequatur laborum doloribus unde alias dignissimos iste hic! Eos ea repudiandae qui quia.
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem dignissimos incidunt harum ut illo aliquam eum, animi libero aperiam fugiat voluptate obcaecati accusantium pariatur consequuntur odio, saepe explicabo accusamus sequi?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, commodi quidem fuga enim illo explicabo! Maxime et consequatur laborum doloribus unde alias dignissimos iste hic! Eos ea repudiandae qui quia.
+      </p>
+    </div>
   </div>
 </template>
+<style lang="scss" scoped>
+  .left-content {
+    background-color: #ffe476;
+  }
+
+  .right-content {
+    background: #fff;
+    padding: 50px;
+    margin-top: 80px;
+    display: block;
+  }
+
+  button {
+    font-size: 1.5em;
+    margin-top: 100px;
+  }
+
+  .subtext {
+    font-size: 1.5em;
+    margin-top: 30px;
+  }
+</style>
